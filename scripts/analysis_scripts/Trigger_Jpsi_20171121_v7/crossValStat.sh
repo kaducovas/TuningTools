0f0701c6e87eeb7c49047d0cef893e3e53c15821
Micael Verissimo <micaelvero@hotmail.com>
2017-12-21 11:14:58 -0200

    cleaning Jpsi

--- scripts/analysis_scripts/Trigger_Jpsi_20171121_v7/crossValStat.sh
+++
@@ -1,8 +0,0 @@
-python crossValStatAnalysis.py \
- -d nn.tuned.pp-N1.hnl0005.hnu0020.sl0000.su0009.il0000.iu0099.et0002.eta0000.pic.gz \
- --outputFileBase 'caonze_analysis.20171012'\
- -rocm 0\
- -modelm 'AUC'\
- --doMatlab 'True'\
- --doMonitoring 'True'
-
--- scripts/analysis_scripts/Trigger_Jpsi_20171121_v7/monitoring.sh
+++
@@ -1,5 +0,0 @@
-crossValStatMonAnalysis.py \
-  -f ./crossValFiles_20170812 \
-  -d ./sample.Jpsi.20172911.npz \
-  --doBeamer 
-#  --choicesfile ~/Treinamento_mc16_LHBining/Xval/lhbinningChoices.mat # escolha da topologia em caso de diversos bins.
