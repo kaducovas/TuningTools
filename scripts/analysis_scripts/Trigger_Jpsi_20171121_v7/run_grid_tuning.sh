9b4635f36c7c6fe9e627e15bb6154e3f3ae00f97
Micael Verissimo <micaelvero@hotmail.com>
2017-12-22 12:21:22 -0200

    make commit to train

--- scripts/analysis_scripts/Trigger_Jpsi_20171121_v7/run_grid_tuning.sh
+++ scripts/analysis_scripts/Trigger_Jpsi_20171121_v7/run_grid_tuning.sh
@@ -5,7 +5,7 @@ python runGRIDtuning.py \
  -pp user.mverissi.new.v4.ppFile.20172112.pic.gz \
  -x user.mverissi.new.v3.crossValid.20172112.pic.gz \
  -r user.mverissi.new.v3.sample.Jpsi.20172911.tight-eff.npz \
- -o user.mverissi.new.output.20172212 \
+ -o user.mverissi.new.v2.output.20172212 \
  --eta-bin 0 4 \
  --et-bin 0 2 \
  --excludedSite ANALY_BNL_MCORE \
