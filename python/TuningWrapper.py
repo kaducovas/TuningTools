d8319234e16b1857189dc61b946ddc8896ac589e
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2018-06-13 19:30:00 -0300

    teste

--- python/TuningWrapper.py
+++ python/TuningWrapper.py
@@ -841,7 +841,7 @@ class TuningWrapper(Logger):
       ########################################################
       history = self._model.fit( self._trnData
                                     , self._trnTarget
-                                    , epochs          = self.trainOptions['nEpochs']
+                                    , epochs          = 1000 #self.trainOptions['nEpochs']
                                     , batch_size      = self.batchSize
                                     #, callbacks       = [self._historyCallback, self._earlyStopping]
                                     , callbacks       = [self._earlyStopping]
