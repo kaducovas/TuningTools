9d2121aa59b1f20bbc732be8ddc011ca1e332da9
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2018-08-06 23:40:15 -0300

    teste

--- python/TuningJob.py
+++ python/TuningJob.py
@@ -1696,7 +1696,7 @@ class TuningJob(Logger):
         #@@bot = telepot.Bot('578139897:AAEJBs9F21TojbPoXM8SIJtHrckaBLZWkpo')
         if(len(os.listdir(outputDir+'/files/'+tuning_folder_name+'/')) == 12):
           #remove temp file which stores starttime so that all the jobs have the same value
-          os.remove(work_path+ppChain.shortName()+".txt")
+          #@@os.remove(work_path+ppChain.shortName()+".txt")
           #subprocess.call("mv "+work_path+ppChain.shortName()+".txt "+work_path+"old/")
           #bot = telepot.Bot('578139897:AAEJBs9F21TojbPoXM8SIJtHrckaBLZWkpo')
           #@@bot_message = ppChain.shortName()+'\nFinished all Jobs for '+fulloutput
