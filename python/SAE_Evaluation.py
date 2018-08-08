12fe8e8c9f2cc9b993ca4bdf61863510d8dadd77
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2018-08-08 20:55:09 -0300

    teste

--- python/SAE_Evaluation.py
+++ python/SAE_Evaluation.py
@@ -757,8 +757,9 @@ def plot_confusion_matrix(cm, classes,
 
 def send_confusion_matrix(fname,dirout,model,y_test,y_pred,points):
   refName,point = points
+  print refName
   print point.thres_value
-  print 'Pd:',point.pd,' SP:',point.sp,' Pf:',point.pf
+  #print 'Pd:',point.pd,' SP:',point.sp,' Pf:',point.pf
   y_pred[y_pred >= point.thres_value] = 1
   y_pred[y_pred < point.thres_value] = -1
   png_files = []
