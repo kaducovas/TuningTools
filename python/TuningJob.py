38947c23656010cb86c0fce640bd3f7f63f22ed7
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2018-08-09 06:30:52 -0300

    teste

--- python/TuningJob.py
+++ python/TuningJob.py
@@ -1580,6 +1580,7 @@ class TuningJob(Logger):
                 time.sleep(int(13*int(sort)))
                 'HEEEEEELP'
                 trnMetrics=report_performance(trnTarget, trnOutput, elapsed=model_time, model_name=ppChain.shortName()+"_"+mname,hl_neuron=neuron,time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Train',points=opPoint,fine_tuning=fine_tuning,report=True)
+                time.sleep(10)
                 valMetrics=report_performance(valTarget, valOutput, elapsed=model_time, model_name=ppChain.shortName()+"_"+mname,hl_neuron=neuron,time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation',points=tstPoint,fine_tuning=fine_tuning,report=True)
 
               elif concat:
