c8e0c7e61ed8bd11850352df723fe2927ca3b493
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2018-08-14 22:02:19 -0300

    teste

--- python/SAE_Evaluation.py
+++ python/SAE_Evaluation.py
@@ -816,32 +816,32 @@ def reconstruct_performance(norm1Par=None,reconstruct=None,model_name="",time=No
       unnorm_reconstruct_val_Data = np.concatenate( unnorm_reconstruct, axis=0 )
       beforenorm_val_Data = np.concatenate( beforenorm, axis=0 )
       ##ALL LABELS
-      ###metrics['Class'] = 'All'
-      ###metrics['Model'] = model_name
-      ###metrics['time'] = time
-      ###metrics['Measure'] = 'Normalized_MI'
+      metrics['Class'] = 'All'
+      metrics['Model'] = model_name
+      metrics['time'] = time
+      metrics['Measure'] = 'Normalized_MI'
       #metrics['HL_Neuron'] = hl_neuron
-      ###metrics['sort'] = sort
-      ###metrics['etBinIdx'] = etBinIdx
-      ###metrics['etaBinIdx'] = etaBinIdx
-      ###metrics['phase'] = phase
+      metrics['sort'] = sort
+      metrics['etBinIdx'] = etBinIdx
+      metrics['etaBinIdx'] = etaBinIdx
+      metrics['phase'] = phase
       #metrics['Elapsed'] = elapsed
       #metrics['fine_tuning'] = fine_tuning
 
-      ###for anel in range(100):
+      for anel in range(100):
         ###print anel
         #print beforenorm_val_Data[:,anel].shape,unnorm_reconstruct_val_Data[:,anel].shape
-        ###try:
-          ###rr = calc_MI2(beforenorm_val_Data[:,anel],unnorm_reconstruct_val_Data[:,anel])
+        try:
+          rr = calc_MI2(beforenorm_val_Data[:,anel],unnorm_reconstruct_val_Data[:,anel])
           #pdf = drv.information_mutual(rr)[0][1]
-          ###mi_score = np.sqrt(1. - np.exp(-2 * rr))
+          mi_score = np.sqrt(1. - np.exp(-2 * rr))
           #print mi_score
           #kl_tot_sort[nsort] = pdf
-          ###metrics[str(anel+1)] = mi_score
-        ###except:
-          ###print 'Anel '+str(anel)+' apresenta erros de calculo'
-          ###metrics[str(anel+1)] = None
-      ###table.insert(metrics)
+          metrics[str(anel+1)] = mi_score
+        except:
+          print 'Anel '+str(anel)+' apresenta erros de calculo'
+          metrics[str(anel+1)] = None
+      table.insert(metrics)
 
       ###metrics = OrderedDict()
       ###print "SIGNAL"
