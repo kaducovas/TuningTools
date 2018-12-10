490f98893a7f912c5190257538f3696690270f6d
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2018-12-10 18:04:50 -0200

    teste

--- python/SAE_Evaluation.py
+++ python/SAE_Evaluation.py
@@ -753,9 +753,10 @@ def getPCAReconstruct(data=None,model=None):
     ret = []
     for i, cdata in enumerate(data):
       ret.append( self._pca.inverse_transform(cdata) )
-   else:
-     ret = self._pca.inverse_transform(cdata)
-   reconstruct[data[0].shape[1] = ret
+  else:
+    ret = self._pca.inverse_transform(cdata)
+  reconstruct[data[0].shape[1] = ret
+  return reconstruct
 
 def getReconstruct(fname,data,sort):
   #from SAE_Evaluation import *
