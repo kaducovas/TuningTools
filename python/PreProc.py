73da835f74aaeef1d8bfdab9a5375e3c5cc2d5ae
Carlos Costa <caducovas@service1.smc-default.americas.sgi.com>
2019-02-02 23:15:31 -0200

    teste

--- python/PreProc.py
+++ python/PreProc.py
@@ -1886,7 +1886,7 @@ class RemoveCalSection( PrepObj ):
     elif self.SectionRemoved == "EM2":
       self._listOfrings = [72,73,74,75,76,77,78,79]
     elif self.SectionRemoved == "EM3":
-      self._listOfrings = [80,81,82,83,84,85,86,87] 
+      self._listOfrings = [80,81,82,83,84,85,86,87]
     elif self.SectionRemoved == "EM":
       self._listOfrings = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87]
     elif self.SectionRemoved == "HAD1":
@@ -1896,7 +1896,7 @@ class RemoveCalSection( PrepObj ):
     elif self.SectionRemoved == "HAD3":
       self._listOfrings = [96,97,98,99]
     elif self.SectionRemoved == "HAD":
-      self._listOfringss = [88,89,90,91,92,93,94,95,96,97,98,99]
+      self._listOfrings = [88,89,90,91,92,93,94,95,96,97,98,99]
 
     checkForUnusedVars(d, self._warning )
 
