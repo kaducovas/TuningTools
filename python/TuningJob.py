9d54d263fe2943536c87faf52a134acd39b87f91
Carlos Eduardo Covas Costa <kaducovas@gmail.com>
2019-02-12 09:29:09 -0200

    uncommented lines

--- python/TuningJob.py
+++ python/TuningJob.py
@@ -1932,29 +1932,29 @@ class TuningJob(Logger):
                 bot.sendPhoto('@ringer_tuning',png_f)
           ##REPLOT
 
-          # if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
-          # #if('AE' in str(ppChain.shortName())):
-            # #for layer in reconstruct.keys():
-            # png_files=plot_pdfs(norm1Par=norm1Par,reconstruct=reconstruct,model_name=ppChain.shortName(),time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation', dirout=work_path+'files/'+tuning_folder_name+'/')
-            # for png_file in png_files:
-              # png_f = open(png_file,'rb')
-              # bot.sendDocument('@ringer_tuning',png_f)
+          if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
+          #if('AE' in str(ppChain.shortName())):
+            #for layer in reconstruct.keys():
+            png_files=plot_pdfs(norm1Par=norm1Par,reconstruct=reconstruct,model_name=ppChain.shortName(),time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation', dirout=work_path+'files/'+tuning_folder_name+'/')
+            for png_file in png_files:
+              png_f = open(png_file,'rb')
+              bot.sendDocument('@ringer_tuning',png_f)
 
-          # if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
-          # #if('AE' in str(ppChain.shortName())):
-            # #for layer in reconstruct.keys():
-            # png_files=plot_pdfs_byclass(norm1Par=norm1Par,reconstruct=reconstruct,model_name=ppChain.shortName(),time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation', dirout=work_path+'files/'+tuning_folder_name+'/')
-            # for png_file in png_files:
-              # png_f = open(png_file,'rb')
-              # bot.sendDocument('@ringer_tuning',png_f)
+          if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
+          #if('AE' in str(ppChain.shortName())):
+            #for layer in reconstruct.keys():
+            png_files=plot_pdfs_byclass(norm1Par=norm1Par,reconstruct=reconstruct,model_name=ppChain.shortName(),time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation', dirout=work_path+'files/'+tuning_folder_name+'/')
+            for png_file in png_files:
+              png_f = open(png_file,'rb')
+              bot.sendDocument('@ringer_tuning',png_f)
 
-          # if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
-          # #if('AE' in str(ppChain.shortName())):
-            # #for layer in reconstruct.keys():
-            # png_files=plot_pdfs_representation(norm1Par=norm1Par,code=code,layer=layer,model_name=ppChain.shortName(),time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation', dirout=work_path+'files/'+tuning_folder_name+'/')
-            # for png_file in png_files:
-              # png_f = open(png_file,'rb')
-              # bot.sendDocument('@ringer_tuning',png_f)
+          if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
+          #if('AE' in str(ppChain.shortName())):
+            #for layer in reconstruct.keys():
+            png_files=plot_pdfs_representation(norm1Par=norm1Par,code=code,layer=layer,model_name=ppChain.shortName(),time=startTime,sort=sort,etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,phase='Validation', dirout=work_path+'files/'+tuning_folder_name+'/')
+            for png_file in png_files:
+              png_f = open(png_file,'rb')
+              bot.sendDocument('@ringer_tuning',png_f)
 
           # # ##HISTO PLOTS
           if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
@@ -1965,27 +1965,27 @@ class TuningJob(Logger):
               png_f = open(png_file,'rb')
               bot.sendDocument('@ringer_tuning',png_f)
 
-          # if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
-            # #if('AE' in str(ppChain.shortName())):
-            # #for layer in reconstruct.keys()
-            # make_ring_hist(norm1Par=norm1Par,reconstruct=reconstruct,model_name=ppChain.shortName(),layer=layer,time=startTime, etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,log_scale=False, dirout='/scratch/22061a/caducovas/run/plots/')
-            # bot.sendMessage('@ringer_tuning','Finished Plotting Rings Histogram Input X Reconstruction')
-            # print 'Finished Plotting RIngs Histogram Input X Reconstruction'
+          if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
+            #if('AE' in str(ppChain.shortName())):
+            #for layer in reconstruct.keys()
+            make_ring_hist(norm1Par=norm1Par,reconstruct=reconstruct,model_name=ppChain.shortName(),layer=layer,time=startTime, etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,log_scale=False, dirout='/scratch/22061a/caducovas/run/plots/')
+            bot.sendMessage('@ringer_tuning','Finished Plotting Rings Histogram Input X Reconstruction')
+            print 'Finished Plotting RIngs Histogram Input X Reconstruction'
 
-          # if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
-            # #if('AE' in str(ppChain.shortName())):
-            # #for layer in reconstruct.keys()
-            # make_representation_hist(norm1Par=norm1Par,code=code,model_name=ppChain.shortName(),layer=layer,time=startTime, etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,log_scale=False, dirout='/scratch/22061a/caducovas/run/plots/')
-            # #bot.sendMessage('@ringer_tuning','Finished Plotting Rings Histogram Input X Reconstruction')
-            # print 'Finished Plotting Code Histogram Signal X Background'
+          if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
+            #if('AE' in str(ppChain.shortName())):
+            #for layer in reconstruct.keys()
+            make_representation_hist(norm1Par=norm1Par,code=code,model_name=ppChain.shortName(),layer=layer,time=startTime, etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,log_scale=False, dirout='/scratch/22061a/caducovas/run/plots/')
+            #bot.sendMessage('@ringer_tuning','Finished Plotting Rings Histogram Input X Reconstruction')
+            print 'Finished Plotting Code Histogram Signal X Background'
 
-          # if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
-            # #if('AE' in str(ppChain.shortName())):
-            # #for layer in reconstruct.keys()
-            # make_ReconstructionErro_hist(norm1Par=norm1Par,reconstructErrVector=valReconError,model_name=ppChain.shortName(),layer=layer,time=startTime, etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,log_scale=False, dirout='/scratch/22061a/caducovas/run/plots/')
-            # #bot.sendMessage('@ringer_tuning','Finished Plotting Rings Histogram Input X Reconstruction')
-            # print 'Finished Plotting Code Reconstruction Error Signal X Background'
-          # ##UNCOMMENT AFTER FIRST ROUND
+          if('PCA' in str(ppChain.shortName()) or 'AE' in str(ppChain.shortName()) and 'std' not in str(ppChain.shortName())):
+            #if('AE' in str(ppChain.shortName())):
+            #for layer in reconstruct.keys()
+            make_ReconstructionErro_hist(norm1Par=norm1Par,reconstructErrVector=valReconError,model_name=ppChain.shortName(),layer=layer,time=startTime, etBinIdx=etBinIdx,etaBinIdx=etaBinIdx,log_scale=False, dirout='/scratch/22061a/caducovas/run/plots/')
+            #bot.sendMessage('@ringer_tuning','Finished Plotting Rings Histogram Input X Reconstruction')
+            print 'Finished Plotting Code Reconstruction Error Signal X Background'
+          ##UNCOMMENT AFTER FIRST ROUND
 
           # #dl_png_files=plot_classifier_training(work_path+'files/'+tuning_folder_name+'/models/',work_path+'files/'+tuning_folder_name+'/models/')
 
