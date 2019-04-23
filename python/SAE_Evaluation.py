79597b2a1a40d13107113cd4cac86d8dd28e86ba
kaducovas <kaducovas@gmail.com>
2019-04-23 20:07:29 -0300

    fixed simple reconstruction table

--- python/SAE_Evaluation.py
+++ python/SAE_Evaluation.py
@@ -773,15 +773,47 @@ def create_reconstruction_table(model_name,script_time,normed='no'):
       strnorm=''
 
   x.field_names = ["Measure"+strnorm, "ETotal"]
-  x.add_row(['Correlation', str(np.format_float_scientific(a[0][0], precision=3))+' '+str(np.format_float_scientific(a[0][1], precision=3))])
-  x.add_row(['DeltaEnergy', str(np.format_float_scientific(a[1][0], precision=3))+' '+str(np.format_float_scientific(a[1][1], precision=3))])
-  x.add_row(['KLDiv', str(np.format_float_scientific(a[2][0], precision=3))+' '+str(np.format_float_scientific(a[2][1], precision=3))])
-  x.add_row(['MI', str(np.format_float_scientific(a[3][0], precision=3))+' '+str(np.format_float_scientific(a[3][1], precision=3))])
-  x.add_row(['MSE', str(np.format_float_scientific(a[4][0], precision=3))+' '+str(np.format_float_scientific(a[4][1], precision=3))])
-  x.add_row(['Norm DeltaEnergy', str(np.format_float_scientific(a[5][1], precision=3))+' '+str(np.format_float_scientific(a[5][1], precision=3))])
-  x.add_row(['Norm MI', str(np.format_float_scientific(a[6][0], precision=3))+' '+str(np.format_float_scientific(a[6][1], precision=3))])
-  x.add_row(['Wasserstein', str(np.format_float_scientific(a[7][0], precision=3))+' '+str(np.format_float_scientific(a[7][1], precision=3))])
-  x.add_row(['KS', str(np.format_float_scientific(a[8][0], precision=3))+' '+str(np.format_float_scientific(a[8][1], precision=3))])
+  x.add_row(['Correlation', str(np.format_float_scientific(a[0][0], precision=3))+' '+str(np.format_float_scientific(a[0][1], precision=1))])
+  x.add_row(['DeltaEnergy', str(np.format_float_scientific(a[1][0], precision=3))+' '+str(np.format_float_scientific(a[1][1], precision=1))])
+  x.add_row(['KLDiv', str(np.format_float_scientific(a[2][0], precision=3))+' '+str(np.format_float_scientific(a[2][1], precision=1))])
+  x.add_row(['MI', str(np.format_float_scientific(a[3][0], precision=3))+' '+str(np.format_float_scientific(a[3][1], precision=1))])
+  x.add_row(['MSE', str(np.format_float_scientific(a[4][0], precision=3))+' '+str(np.format_float_scientific(a[4][1], precision=1))])
+  x.add_row(['Norm DeltaEnergy', str(np.format_float_scientific(a[5][0], precision=3))+' '+str(np.format_float_scientific(a[5][1], precision=1))])
+  x.add_row(['Norm MI', str(np.format_float_scientific(a[6][0], precision=3))+' '+str(np.format_float_scientific(a[6][1], precision=1))])
+  x.add_row(['Wasserstein', str(np.format_float_scientific(a[7][0], precision=3))+' '+str(np.format_float_scientific(a[7][1], precision=1))])
+  x.add_row(['KS', str(np.format_float_scientific(a[8][0], precision=3))+' '+str(np.format_float_scientific(a[8][1], precision=1))])
+  return x
+
+def create_reconstruction_table_complete(model_name,script_time,normed='no'):
+  #from SAE_Evaluation import *
+  from prettytable import PrettyTable
+  import sqlite3
+  from decimal import Decimal
+  cnx=sqlite3.connect('//home/caducovas/run/ringerMLlab.db')
+  #df = pd.read_sql_query("select point,sort,100*round(sp,4) as sp, 100*round(pd,4) as pd, 100*round(pf,4) as pf, 100*round(f1,4) as f1, 100*round(auc,4) as auc, 100*round(precision,4) as precision,100*round(recall,4) as recall from classifiers where model = '"+model_name+"' and time = '"+script_time+"' and phase = 'Validation'",cnx)
+  df = pd.read_sql_query("select measure, ETotal,EM,HAD from reconstruction_metrics  where class= 'All' and normed = '"+normed+"' and model = '"+model_name[:-1]+"' and time = '"+script_time+"' order by Measure",cnx)
+  #df = pd.read_sql_query("select point,sort,100*round(sp,4) as sp, 100*round(pd,4) as pd, 100*round(pf,4) as pf, 100*round(f1,4) as f1, 100*round(auc,4) as auc, 100*round(precision,4) as precision,100*round(recall,4) as recall from classifiers where id in (select id from (select max(sp) as maxsp,id from classifiers where model = '"+model_name+"' and time = '"+script_time+"' and phase = 'Validation' group by Point,Model,HL_Neuron,time,sort,etBinIdx,etaBinIdx,phase,fine_tuning))",cnx)
+  #df['ETotal'] = df['ETotal'].apply(lambda x: '%.4E' % Decimal(x))
+  print df
+
+  a = df.groupby(['Measure']).agg({'ETotal':['mean','std'],'EM':['mean','std'],'HAD':['mean','std']}).values
+  #df_values = np.round(a,2)
+  x = PrettyTable()
+  if normed=='yes':
+      strnorm='(Normed)'
+  else:
+      strnorm=''
+
+  x.field_names = ["Measure"+strnorm, "ETotal", "EM", "HAD"]
+  x.add_row(['Correlation', str(np.format_float_scientific(a[0][2], precision=1)),str(np.format_float_scientific(a[0][0], precision=1)),str(np.format_float_scientific(a[0][4], precision=1))])
+  x.add_row(['DeltaEnergy', str(np.format_float_scientific(a[1][2], precision=1)),str(np.format_float_scientific(a[1][0], precision=1)),str(np.format_float_scientific(a[1][4], precision=1))])
+  x.add_row(['KLDiv', str(np.format_float_scientific(a[2][2], precision=1)),str(np.format_float_scientific(a[2][0], precision=1)),str(np.format_float_scientific(a[2][4], precision=1))])
+  x.add_row(['MI', str(np.format_float_scientific(a[3][2], precision=1)),str(np.format_float_scientific(a[3][0], precision=1)),str(np.format_float_scientific(a[3][4], precision=1))])
+  x.add_row(['MSE', str(np.format_float_scientific(a[4][2], precision=1)),str(np.format_float_scientific(a[4][0], precision=1)),str(np.format_float_scientific(a[4][4], precision=1))])
+  x.add_row(['Norm DeltaEnergy', str(np.format_float_scientific(a[5][2], precision=1)),str(np.format_float_scientific(a[5][0], precision=1)),str(np.format_float_scientific(a[5][4], precision=1))])
+  x.add_row(['Norm MI', str(np.format_float_scientific(a[6][2], precision=1)),str(np.format_float_scientific(a[6][0], precision=1)),str(np.format_float_scientific(a[6][4], precision=1))])
+  x.add_row(['Wasserstein', str(np.format_float_scientific(a[7][2], precision=1)),str(np.format_float_scientific(a[7][0], precision=1)),str(np.format_float_scientific(a[7][4], precision=1))])
+  x.add_row(['KS', str(np.format_float_scientific(a[8][2], precision=1)),str(np.format_float_scientific(a[8][0], precision=1)),str(np.format_float_scientific(a[8][4], precision=1))])
   return x
 
 def create_simple_table(model_name,script_time):
--- python/TuningJob.py
+++ python/TuningJob.py
@@ -1880,6 +1880,7 @@ class TuningJob(Logger):
           bot.sendMessage('@ringer_tuning',create_simple_table(ppChain.shortName()+"_"+mname,startTime).get_string())
           bot.sendMessage('@ringer_tuning',create_reconstruction_table(ppChain.shortName()+"_"+mname,startTime,normed='no').get_string())
           bot.sendMessage('@ringer_tuning',create_reconstruction_table(ppChain.shortName()+"_"+mname,startTime,normed='yes').get_string())
+          bot.sendMessage('@ringer_tuning',create_reconstruction_table_complete(ppChain.shortName()+"_"+mname,startTime,normed='no').get_string())
 
           # if('AE' in str(ppChain.shortName()) and ('LSTM' not in str(ppChain.shortName()) and 'GRU' not in str(ppChain.shortName()))):
           #   png_files=plot_AE_training(work_path+'StackedAutoEncoder_preproc/'+tuning_folder_name,work_path+'files/'+tuning_folder_name+'/')
